a554b8cd1429ca9ccdfa967a411803678da9ef8d
oglz <alesouzadn@gmail.com>
2022-03-08 21:43:00 -0300

    Exercício introdutório de Kotlin.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    //Roteiro para futuro filme
+    println("Henrique X Nerdmaster part 1")
+    println("Nerdmaster: Finalmente, um oponente digno.")
+    println("Henrique: Quem é você?")
+    println("Nerdmaster: Eu sou você... só que senior.")
+    println("Henrique: você se corrompeu ao usar o poder do kotlin")
+    println("Nerdmaster: VOCÊ É FRACO! LHE FALTA LINHAS DE CÓDIGO.")
+    println("Henrique: mas.. eu tenho um github personalizado com jogo da cobrinha.")
+    println("Nerdmaster: O Uso de Chrome lhe enfraquece henrique, não limite o seu poder a tão pouco.")
+    println("Henrique: Pense nos seus alunos, o que eles irão pensar quando descobrirem que você se corrompeu a todo esse poder? ")
+    println("Nerdmaster: Meus alunos se foram a muito tempo, Liza, Generation, Todos ficaram pra trás.")
+    println("Henrique: O QUE VOCÊ FEZ ?")
+    println("Nerdmaster: Usei o código corrompido, descobri a força por trás do uso restrito do chrome, desafie-se e encontre a verdade. ")
+    println("Henrique: Olhe para você, se corromepeu por tão pouco, e o que você tem agora? onde estão todos que lhe ajudaram a chegar até aqui ?")
+    println("Nerdmaster: Nunca precisei deles. eram fracos, e limitavam o meu poder.")
+    println("Nerdmaster: Ignorancia, é FATAL! MUAHAHAHA")
+    println("Henrique acorda assustado")
+    println("Henrique: Ufa, foi apenas um sonho.")
+    println("CONTINUA.")
+
+}
